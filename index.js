8ae3347bbf7e5901c98532ed4f7733441f6334cc
Ameet Kaustav <akaustav@users.noreply.github.com>
2018-09-16 23:57:54 -0700

    + changes

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const couchbase = require("couchbase");
-const cluster = new couchbase.Cluster("couchbase://localhost");
-cluster.authenticate("nodejs", "test123");
-const bucket = cluster.openBucket("ameet");
+const couchbase = require('couchbase');
+const cluster = new couchbase.Cluster('couchbase://192.168.86.79');
+cluster.authenticate('nodejs', 'test123');
+const BUCKET_NAME = 'ameet';
+
+const bucket = cluster.openBucket('ameet');
+console.info(`Established connection to couchbase bucket ${BUCKET_NAME}`);
+
 const N1qlQuery = couchbase.N1qlQuery;
 
 bucket.manager().createPrimaryIndex(function() {
   bucket.upsert(
-    "user:king_arthur",
+    'user:king_arthur',
     {
-      email: "kingarthur@couchbase.com",
-      interests: ["Holy Grail", "African Swallows"]
+      email: 'kingarthur@couchbase.com',
+      interests: ['Holy Grail', 'African Swallows']
     },
     function(err, result) {
-      bucket.get("user:king_arthur", function(err, result) {
-        console.log("Got result: %j", result.value);
+      bucket.get('user:king_arthur', function(err, result) {
+        if (err) {
+          throw err;
+        }
+        console.log('Got result: %j', result.value);
         bucket.query(
           N1qlQuery.fromString(
-            "SELECT * FROM ameet WHERE $1 in interests LIMIT 1"
+            'SELECT * FROM ameet WHERE $1 in interests LIMIT 1'
           ),
-          ["African Swallows"],
+          ['African Swallows'],
           function(err, rows) {
-            console.log("Got rows: %j", rows);
+            if (err) {
+              throw err;
+            }
+            console.log('Got rows: %j', rows);
           }
         );
       });
